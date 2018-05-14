cb294db3dfca5dd1322f62c91c47d02f6a3671b7
Emmeran Seehuber <rototor@rototor.de>
2018-05-14 13:49:01 +0200

    Made the benchmark parameterized

--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/graphics/image/LosslessFactoryBenchmark.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/graphics/image/LosslessFactoryBenchmark.java
@@ -1,11 +1,5 @@
 package org.apache.pdfbox.pdmodel.graphics.image;
 
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
 import org.apache.pdfbox.filter.Filter;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.openjdk.jmh.annotations.*;
@@ -14,140 +8,100 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 @State(Scope.Benchmark)
 @Warmup(iterations = 5, time = 1)
 @Measurement(iterations = 5, time = 4)
 @Threads(1)
 public class LosslessFactoryBenchmark
 {
-	private BufferedImage imgSmall;
-	private PDDocument doc;
-	private BufferedImage imgBig;
-	private BufferedImage imgBigBytes;
+    private BufferedImage imgSmall;
+    private PDDocument doc;
+    private BufferedImage imgBig;
+    private BufferedImage imgBigBytes;
 
-	@Setup
-	public void setupBenchmark() throws IOException
-	{
-		System.setProperty("sun.java2d.cmm", "sun.java2d.cmm.kcms.KcmsServiceProvider");
-		imgSmall = ImageIO.read(LosslessFactory.class.getResourceAsStream("png.png"));
-		imgBig = new BufferedImage(imgSmall.getWidth() * 10, imgSmall.getHeight() * 10, BufferedImage.TYPE_INT_RGB);
-		imgBigBytes = new BufferedImage(imgSmall.getWidth() * 10, imgSmall.getHeight() * 10, BufferedImage.TYPE_3BYTE_BGR);
+    @Setup
+    public void setupBenchmark() throws IOException
+    {
+        System.setProperty("sun.java2d.cmm", "sun.java2d.cmm.kcms.KcmsServiceProvider");
+        imgSmall = ImageIO.read(LosslessFactory.class.getResourceAsStream("png.png"));
+        imgBig = new BufferedImage(imgSmall.getWidth() * 10, imgSmall.getHeight() * 10,
+                BufferedImage.TYPE_INT_RGB);
+        imgBigBytes = new BufferedImage(imgSmall.getWidth() * 10, imgSmall.getHeight() * 10,
+                BufferedImage.TYPE_3BYTE_BGR);
 
-		Graphics2D graphics = imgBig.createGraphics();
-		graphics.drawImage(imgSmall, 0, 0, imgBig.getWidth(), imgBig.getWidth(), null);
-		graphics.dispose();
+        Graphics2D graphics = imgBig.createGraphics();
+        graphics.drawImage(imgSmall, 0, 0, imgBig.getWidth(), imgBig.getWidth(), null);
+        graphics.dispose();
 
-		graphics = imgBigBytes.createGraphics();
-		graphics.drawImage(imgSmall, 0, 0, imgBigBytes.getWidth(), imgBigBytes.getWidth(), null);
-		graphics.dispose();
+        graphics = imgBigBytes.createGraphics();
+        graphics.drawImage(imgSmall, 0, 0, imgBigBytes.getWidth(), imgBigBytes.getWidth(), null);
+        graphics.dispose();
 
-		doc = new PDDocument();
-	}
+        doc = new PDDocument();
+    }
 
-	@Benchmark()
-	public PDImageXObject rgbOnly9() throws IOException
-	{
-		System.setProperty(Filter.SYSPROP_DEFLATELEVEL, "9");
-		LosslessFactory.usePredictorEncoder = false;
-		return LosslessFactory.createFromImage(doc, imgSmall);
-	}
+    @Param({ "3", "6", "9" })
+    public String zipLevel;
 
-	@Benchmark
-	public PDImageXObject predictor9() throws IOException
-	{
-		System.setProperty(Filter.SYSPROP_DEFLATELEVEL, "9");
-		LosslessFactory.usePredictorEncoder = true;
-		return LosslessFactory.createFromImage(doc, imgSmall);
-	}
-	
-	@Benchmark()
-	public PDImageXObject rgbOnly6() throws IOException
-	{
-		System.setProperty(Filter.SYSPROP_DEFLATELEVEL, "6");
-		LosslessFactory.usePredictorEncoder = false;
-		return LosslessFactory.createFromImage(doc, imgSmall);
-	}
+    @Benchmark()
+    public PDImageXObject rgbOnly() throws IOException
+    {
+        System.setProperty(Filter.SYSPROP_DEFLATELEVEL, zipLevel);
+        LosslessFactory.usePredictorEncoder = false;
+        return LosslessFactory.createFromImage(doc, imgSmall);
+    }
 
-	@Benchmark
-	public PDImageXObject predictor6() throws IOException
-	{
-		System.setProperty(Filter.SYSPROP_DEFLATELEVEL, "9");
-		LosslessFactory.usePredictorEncoder = true;
-		return LosslessFactory.createFromImage(doc, imgSmall);
-	}
+    @Benchmark
+    public PDImageXObject predictor() throws IOException
+    {
+        System.setProperty(Filter.SYSPROP_DEFLATELEVEL, zipLevel);
+        LosslessFactory.usePredictorEncoder = true;
+        return LosslessFactory.createFromImage(doc, imgSmall);
+    }
 
+    @Benchmark()
+    public PDImageXObject rgbOnlyBig() throws IOException
+    {
+        System.setProperty(Filter.SYSPROP_DEFLATELEVEL, zipLevel);
+        LosslessFactory.usePredictorEncoder = false;
+        return LosslessFactory.createFromImage(doc, imgBig);
+    }
 
-	@Benchmark()
-	public PDImageXObject rgbOnly9Big() throws IOException
-	{
-		System.setProperty(Filter.SYSPROP_DEFLATELEVEL, "9");
-		LosslessFactory.usePredictorEncoder = false;
-		return LosslessFactory.createFromImage(doc, imgBig);
-	}
+    @Benchmark
+    public PDImageXObject predictorBig() throws IOException
+    {
+        System.setProperty(Filter.SYSPROP_DEFLATELEVEL, zipLevel);
+        LosslessFactory.usePredictorEncoder = true;
+        return LosslessFactory.createFromImage(doc, imgBig);
+    }
 
-	@Benchmark
-	public PDImageXObject predictor9Big() throws IOException
-	{
-		System.setProperty(Filter.SYSPROP_DEFLATELEVEL, "9");
-		LosslessFactory.usePredictorEncoder = true;
-		return LosslessFactory.createFromImage(doc, imgBig);
-	}
+    @Benchmark()
+    public PDImageXObject rgbOnlyBigBytes() throws IOException
+    {
+        System.setProperty(Filter.SYSPROP_DEFLATELEVEL, zipLevel);
+        LosslessFactory.usePredictorEncoder = false;
+        return LosslessFactory.createFromImage(doc, imgBigBytes);
+    }
 
-	@Benchmark()
-	public PDImageXObject rgbOnly6Big() throws IOException
-	{
-		System.setProperty(Filter.SYSPROP_DEFLATELEVEL, "6");
-		LosslessFactory.usePredictorEncoder = false;
-		return LosslessFactory.createFromImage(doc, imgBig);
-	}
+    @Benchmark
+    public PDImageXObject predictorBigBytes() throws IOException
+    {
+        System.setProperty(Filter.SYSPROP_DEFLATELEVEL, zipLevel);
+        LosslessFactory.usePredictorEncoder = true;
+        return LosslessFactory.createFromImage(doc, imgBigBytes);
+    }
 
-	@Benchmark
-	public PDImageXObject predictor6Big() throws IOException
-	{
-		System.setProperty(Filter.SYSPROP_DEFLATELEVEL, "9");
-		LosslessFactory.usePredictorEncoder = true;
-		return LosslessFactory.createFromImage(doc, imgBig);
+    public static void main(String[] args) throws RunnerException
+    {
+        Options opt = new OptionsBuilder()
+                .include(".*" + LosslessFactoryBenchmark.class.getSimpleName() + ".*").forks(1)
+                .build();
 
-	}
-
-	@Benchmark()
-	public PDImageXObject rgbOnly9BigBytes() throws IOException
-	{
-		System.setProperty(Filter.SYSPROP_DEFLATELEVEL, "9");
-		LosslessFactory.usePredictorEncoder = false;
-		return LosslessFactory.createFromImage(doc, imgBigBytes);
-	}
-
-	@Benchmark
-	public PDImageXObject predictor9BigBytes() throws IOException
-	{
-		System.setProperty(Filter.SYSPROP_DEFLATELEVEL, "9");
-		LosslessFactory.usePredictorEncoder = true;
-		return LosslessFactory.createFromImage(doc, imgBigBytes);
-	}
-
-	@Benchmark()
-	public PDImageXObject rgbOnly6BigBytes() throws IOException
-	{
-		System.setProperty(Filter.SYSPROP_DEFLATELEVEL, "6");
-		LosslessFactory.usePredictorEncoder = false;
-		return LosslessFactory.createFromImage(doc, imgBigBytes);
-	}
-
-	@Benchmark
-	public PDImageXObject predictor6BigBytes() throws IOException
-	{
-		System.setProperty(Filter.SYSPROP_DEFLATELEVEL, "9");
-		LosslessFactory.usePredictorEncoder = true;
-		return LosslessFactory.createFromImage(doc, imgBigBytes);
-	}
-
-	public static void main(String[] args) throws RunnerException
-	{
-		Options opt = new OptionsBuilder()
-				.include(".*" + LosslessFactoryBenchmark.class.getSimpleName() + ".*")
-				.forks(1).build();
-
-		new Runner(opt).run();
-	}
+        new Runner(opt).run();
+    }
 }
